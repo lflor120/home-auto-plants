298d1171fd187bae9bc67c6550f05bebc4ecaaef
Leonardo Flores <leonard4flores@gmail.com>
2024-10-20 17:39:42 -0500

    creating patch models for user and plant

--- backend/app/schemas.py
+++ backend/app/schemas.py
@@ -18,6 +18,23 @@ class UserCreate(BaseModel):
         populate_by_name=True
     )
 
+class UserPatch(BaseModel):
+    email: str | None = None
+    username: str | None = None
+    name: str | None = None
+    password: str | None = None
+
+    @field_validator('email')
+    def must_be_valid_email(cls, value: str):
+        if "@" not in value:
+            raise ValueError("Must be a valid email")
+        return value
+    
+    model_config = ConfigDict(
+        alias_generator=to_camel,
+        populate_by_name=True
+    )
+
 class User(UserCreate):
     id: int
 
@@ -31,6 +48,18 @@ class PlantCreate(BaseModel):
     watering_schedule: str
     plant_name: str
     description: str
+
+    model_config = ConfigDict(
+        alias_generator=to_camel,
+        populate_by_name=True
+    )
+
+class PlantPatch(BaseModel):
+    owner_id: int | None = None
+    plant_type: str | None = None
+    watering_schedule: str | None = None
+    plant_name: str | None = None
+    description: str | None = None
 
     model_config = ConfigDict(
         alias_generator=to_camel,
