910967137f41e242a631f866d225734132a04402
Leonardo Flores <leonard4flores@gmail.com>
2024-10-20 15:04:34 -0500

    adding routers to main

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,7 +1,16 @@
 from fastapi import FastAPI
 
+from . import models
+from .database import engine
+from .routers import plant, user
+
+models.Base.metadata.create_all(bind=engine)
+
 app = FastAPI()
 
 @app.get("/")
 def get_home():
     return {"message": "Welcome to Plant Automation!"}
+
+app.include_router(plant.router)
+app.include_router(user.router)
