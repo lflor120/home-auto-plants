1b6b4fd0b256e27fa77ba02e41a5e55811e01d3a
Leonardo Flores <leonard4flores@gmail.com>
2024-12-25 12:45:04 -0600

    frontend changes

--- frontend/src/pages/plant/add.tsx
+++ frontend/src/pages/plant/add.tsx
@@ -3,54 +3,56 @@ import { useState } from 'react';
 import { Form, Button, Container, Row, Col, FloatingLabel } from 'react-bootstrap';
 import Select from 'react-select';
 import CreatableSelect from 'react-select/creatable';
+const BACKEND_API = process.env.NEXT_PUBLIC_BACKEND_BASE_URL;
 
 interface Plant {
-    name: string;
-    type: string;
-    interval: string;
+    ownerId: string
+    plantName: string;
+    plantType: string;
+    interval: number | null;
     frequency: string;
     description: string;
 }
 
 interface selectOption {
     name: string | null;
-    value: string | null;
+    // value: string | null;
     label: string | null;
 }
 
-const plantOptions : selectOption[] = [
-    { name: "type", value: "chinese-money-plant", label: "Chinese Money Plant" },
-    { name: "type", value: "snake-grass", label: "Snake Grass" },
-    { name: "type", value: "prayer-plant", label: "Prayer Plant" },
-    { name: "type", value: "snare-plant", label: "Snare Plant" }
+const plantOptions: selectOption[] = [
+    { name: "plantType", label: "Chinese Money Plant" },
+    { name: "plantType", label: "Snake Grass" },
+    { name: "plantType", label: "Prayer Plant" },
+    { name: "plantType", label: "Snare Plant" }
 ]
-const frequencyOptions : selectOption[] = [
-    { name: "frequency", value: "daily", label: "Daily" },
-    { name: "frequency", value: "weekly", label: "Weekly" },
-    { name: "frequency", value: "monthly", label: "Monthly" },
+const frequencyOptions: selectOption[] = [
+    { name: "frequency", label: "Daily" },
+    { name: "frequency", label: "Weekly" },
+    { name: "frequency", label: "Monthly" },
 ]
 
-const interval : selectOption[] = [
-    { name: "interval", value: "1", label: "1" },
-    { name: "interval", value: "2", label: "2" },
-    { name: "interval", value: "3", label: "3" },
-    { name: "interval", value: "4", label: "4" },
-    { name: "interval", value: "5", label: "5" },
-    { name: "interval", value: "6", label: "6" },
-    { name: "interval", value: "7", label: "7" },
+const interval: selectOption[] = [
+    { name: "interval", label: "1" },
+    { name: "interval", label: "2" },
+    { name: "interval", label: "3" },
+    { name: "interval", label: "4" },
+    { name: "interval", label: "5" },
+    { name: "interval", label: "6" },
+    { name: "interval", label: "7" },
 ]
 
 const PlantForm = () => {
     const [plantData, setPlantData] = useState<Plant>({
-        name: '',
-        type: '',
-        interval: '',
-        frequency: '', 
+        ownerId: '',
+        plantName: '',
+        plantType: '',
+        interval: null,
+        frequency: '',
         description: '',
     });
 
     const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
-        console.log(e)
         const { name, value } = e.target;
         setPlantData((prevData) => ({
             ...prevData,
@@ -58,7 +60,7 @@ const PlantForm = () => {
         }));
     };
 
-    const handleSelectChange = (selected: selectOption | any ) => {
+    const handleSelectChange = (selected: selectOption | any) => {
         console.log(selected)
         const { name, value, label } = selected;
 
@@ -68,17 +70,40 @@ const PlantForm = () => {
         }));
     }
 
+    async function postPlant(plant: Plant): Promise<Plant> {
+        try {
+            const response = await fetch(`${BACKEND_API}/plant`, {
+                method: "POST",
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify(plant)
+            });
+            if (!response.ok) {
+                    throw new Error(`Error! status: ${response.status}`);
+            }
+
+            const data: Plant = await response.json();
+            return data;
+            
+        } catch (error) {
+            console.error("Error adding plant:", error);
+            throw error;
+        }
+    }
+
     const handleSubmit = async (e: React.FormEvent) => {
         e.preventDefault();
 
-        // Replace with your actual form submission logic (API call, etc.)
+        // call backend api
+        
         console.log('Plant Data Submitted:', plantData);
+
 
         // Reset the form after submission
         setPlantData({
-            name: '',
-            type: '',
-            interval: '',
+            ownerId: '',
+            plantName: '',
+            plantType: '',
+            interval: null,
             frequency: '',
             description: '',
         });
@@ -93,8 +118,8 @@ const PlantForm = () => {
                     <Form.Label className="fw-bold">Plant Name ✏️</Form.Label>
                     <Form.Control
                         type="text"
-                        name="name"
-                        value={plantData.name}
+                        name="plantName"
+                        value={plantData.plantName}
                         onChange={handleChange}
                         placeholder="Name your plant!"
                         required
@@ -109,6 +134,7 @@ const PlantForm = () => {
                         options={plantOptions}
                         placeholder="What kind of plant do you have?"
                         onChange={handleSelectChange}
+                        instanceId="plant-type-select"
                         required
                     />
                 </Form.Group>
@@ -121,13 +147,14 @@ const PlantForm = () => {
                         <Col>
                             <Form.Group>
                                 <Form.Label className="small">Interval</Form.Label>
-                                <Select 
+                                <Select
                                     id="interval-id"
                                     options={interval}
                                     placeholder="How many times should we water your plant?"
                                     isSearchable={false}
                                     name="interval"
                                     onChange={handleSelectChange}
+                                    instanceId="watering-sched-select"
                                     required
                                 />
                             </Form.Group>
@@ -135,12 +162,13 @@ const PlantForm = () => {
                         <Col>
                             <Form.Group>
                                 <Form.Label className="small">Recur Every</Form.Label>
-                                <Select 
+                                <Select
                                     id="frequency-id"
                                     options={frequencyOptions}
                                     placeholder="How often?"
                                     isSearchable={false}
                                     onChange={handleSelectChange}
+                                    instanceId="recur-select"
                                     required
                                 />
                             </Form.Group>
