47f7ca381fe7e75a61f318be4fcc9ed90eacc62b
Leonardo Flores <leonard4flores@gmail.com>
2025-01-29 00:58:41 -0600

    setting up CORS middleware to accept requests from frontend

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-
+from fastapi.middleware.cors import CORSMiddleware
 from . import models
 from .database import engine
 from .routers import plant
@@ -7,6 +7,16 @@ from .routers import plant
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+origins = ["http://localhost:3000"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,  # List of allowed origins
+    allow_credentials=True,
+    allow_methods=["GET", "POST", "PUT", "DELETE", "PATCH"],  # Allowed HTTP methods
+    allow_headers=["*"],  # Allows all headers
+)
 
 @app.get("/")
 def get_home():
